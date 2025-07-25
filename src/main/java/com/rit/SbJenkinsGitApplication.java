b7e5cdb567200f3c3350e52a2a20c2511c2ad141
Global Developer <ritit.programs@gmail.com>
2025-07-25 23:25:46 +0530

    First Project commit

---
+++ src/main/java/com/rit/SbJenkinsGitApplication.java
@@ -0,0 +1,13 @@
+package com.rit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJenkinsGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJenkinsGitApplication.class, args);
+	}
+
+}
